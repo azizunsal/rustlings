a1c9df0a15324240054ed7effb3548f2ffa0a6dd
Aziz Unsal <azizunsal@Azizs-MacBook-Pro.local>
2022-10-28 12:24:15 +0300

    Completed Option exercises.

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -1,8 +1,6 @@
 // options3.rs
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -12,8 +10,16 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => println!("no match"),
     }
     y; // Fix without deleting this line.
 }
+
+
+// & vs ref
+// & denotes that your pattern expects a reference to an object. Hence & is a part of said pattern: &Foo matches different objects than Foo does.
+//
+// ref indicates that you want a reference to an unpacked value. It is not matched against: Foo(ref foo) matches the same objects as Foo(foo).
+//
+// (https://doc.rust-lang.org/std/keyword.ref.html)
