a7cdcc4e9c1be7db16766e3f45dd1beda72741f7
Aziz Unsal <azizunsal@Azizs-MacBook-Pro.local>
2022-11-21 14:58:55 +0300

    Solved clippy exercise2; used if let over an Option.

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -1,13 +1,18 @@
 // clippy2.rs
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    for x in option {
-        res += x;
-    }
+    // for x in option {
+    //     res += x;
+    // }
+    // println!("{}", res);
+
+    // [aziz]: Clippy produces `for_loops_over_fallibles` error for above code.
+    //         Both lines are correct; `while let` and `if let`
+    //         Explanations are here https://doc.rust-lang.org/rust-by-example/flow_control/if_let.html
+    // while let Some(i) = option {res += i;}
+    if let Some(i) = option { res += i; }
     println!("{}", res);
 }
