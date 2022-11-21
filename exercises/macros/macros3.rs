4db6f88bfa5ecc57e207f549ee23a34c81094c3f
Aziz Unsal <azizunsal@Azizs-MacBook-Pro.local>
2022-11-21 14:30:17 +0300

    Solved the macros exercises.

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -2,9 +2,9 @@
 // Make me compile, without taking the macro out of the module!
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 mod macros {
+    // [aziz] In order to use a macro outside of its module, macro should be exported.
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
